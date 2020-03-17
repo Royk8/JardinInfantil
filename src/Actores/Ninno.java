e7d31ce59991ab09e5cd2871862b7fdd45ef4c84
JhonierCordoba <jhonier.cordoba@udea.edu.co>
2020-03-17 16:37:28 -0500

    se cambió la clase niño y se agrego la clase logro

--- src/Actores/Ninno.java
+++ src/Actores/Ninno.java
@@ -1,59 +1,30 @@
 package Actores;
 
+import java.util.Date;
+
 public class Ninno extends Persona{
     private int edad;
     private float talla, peso;
-    private String situacionEspecial, genero;
+    private String situacionEspecial, nivel;
+    private char genero, horario;
+    private Date fechaDeNacimiento;
+    private Arraylist<Logro> logros;
 
-    public Ninno(long id, String nombre, String idType, int edad, float talla, float peso, String situacionEspecial, String genero) {
+    public Ninno(long id, String nombre, String idType, int edad, float talla, float peso,
+                 String situacionEspecial, String nivel, char genero, char horario, Date fechaDeNacimiento,
+                 Arraylist<Logro> logros) {
+
         super(id, nombre, idType);
         this.edad = edad;
         this.talla = talla;
         this.peso = peso;
         this.situacionEspecial = situacionEspecial;
+        this.nivel = nivel;
         this.genero = genero;
+        this.horario = horario;
+        this.fechaDeNacimiento = fechaDeNacimiento;
+        this.logros = logros;
+
     }
 
-    public Ninno(){
-    }
-
-    public int getEdad() {
-        return edad;
-    }
-
-    public void setEdad(int edad) {
-        this.edad = edad;
-    }
-
-    public float getTalla() {
-        return talla;
-    }
-
-    public void setTalla(float talla) {
-        this.talla = talla;
-    }
-
-    public float getPeso() {
-        return peso;
-    }
-
-    public void setPeso(float peso) {
-        this.peso = peso;
-    }
-
-    public String getSituacionEspecial() {
-        return situacionEspecial;
-    }
-
-    public void setSituacionEspecial(String situacionEspecial) {
-        this.situacionEspecial = situacionEspecial;
-    }
-
-    public String getGenero() {
-        return genero;
-    }
-
-    public void setGenero(String genero) {
-        this.genero = genero;
-    }
 }
